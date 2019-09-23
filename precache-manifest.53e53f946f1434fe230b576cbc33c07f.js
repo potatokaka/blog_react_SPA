528cf0004dc4c275ac5f9244329b6cfaf840ef7e
Kaka <potatokaka@gmail.com>
2019-09-23 14:04:52 +0800

    first

---
+++ precache-manifest.53e53f946f1434fe230b576cbc33c07f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c36d79cea5ce1c29d86aba813d6bf43d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "96f631c9504a6ef154ff",
+    "url": "./static/css/main.3b9aff55.chunk.css"
+  },
+  {
+    "revision": "2871490a36de1c720156",
+    "url": "./static/js/2.75bda3d1.chunk.js"
+  },
+  {
+    "revision": "96f631c9504a6ef154ff",
+    "url": "./static/js/main.60a479e7.chunk.js"
+  },
+  {
+    "revision": "e1b5a27b678cb9d80f25",
+    "url": "./static/js/runtime~main.04a13e19.js"
+  }
+]);
